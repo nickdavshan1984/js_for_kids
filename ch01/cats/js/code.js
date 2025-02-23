4b47c999bfa338070da60e7203491d56790e605a
Nick Davshan <spacestark84@gmail.com>
2025-02-23 10:39:06 +0200

    Update code.js

--- ch01/cats/js/code.js
+++ ch01/cats/js/code.js
@@ -1,1 +1,7 @@
-alert('Cats')
+// Малюємо стільки котиків, скільки захочемо!
+var drawCats = function (howManyTimes) {
+    for (var i = 0; i < howManyTimes; i++) {
+        console.log(i + " =^.^=");
+    }
+};
+drawCats(10);
