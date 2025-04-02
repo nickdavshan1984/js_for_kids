18072684d16847735464808cc5e103a39de1ef37
Nick Davshan <spacestark84@gmail.com>
2025-04-02 13:40:39 +0300

    add.array.files

---
+++ ch03/creating_an_array/js/code.js
@@ -0,0 +1,14 @@
+//dinosours array
+var dinosaurs = [
+                "T-rex", 
+                "Velociraptor", 
+                "Stegosaurus",  
+                "Triceratops",
+                "Brachiosaurus", 
+                "Pteranodon", 
+                "Apatosaurus",
+                "Diplodocus", 
+                "Compsognathus"  
+];
+
+console.log(dinosaurs);
