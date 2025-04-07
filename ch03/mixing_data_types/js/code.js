1f362cb02d883c50e553bc00a88fe86b89bb2819
Nick Davshan <spacestark84@gmail.com>
2025-04-07 12:23:17 +0300

    add.new_arrays.files

---
+++ ch03/mixing_data_types/js/code.js
@@ -0,0 +1,6 @@
+//dinosours and numbers
+var dinosoursAndNumbers = [3, "dinosaurus", ["triceratops", "stegosaurus", 3627.5], 10];
+
+console.log(dinosoursAndNumbers[2]);
+console.log(dinosoursAndNumbers[2][0]);
+
---
+++ ch03/setting_or_changing_elements/js/code.js
@@ -0,0 +1,18 @@
+//dinosours array
+var dinosaurs = [
+                "T-rex", 
+                "Velociraptor", 
+                "Stegosaurus",  
+                "Triceratops",
+                "Brachiosaurus", 
+                "Pteranodon", 
+                "Apatosaurus",
+                "Diplodocus", 
+                "Compsognathus"  
+];
+
+dinosaurs[0] = "Tyrannosaurus Rex";
+console.log(dinosaurs);
+
+dinosaurs[33] = "Philosoraptor";
+console.log(dinosaurs);
