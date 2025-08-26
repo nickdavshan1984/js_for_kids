717d3028bf13851c40892ebb0bd1de63987a7c97
Nick Davshan <spacestark84@gmail.com>
2025-08-26 14:20:40 +0300

    back_to_the_very_begin

--- ch01/cats/js/code.js
+++
@@ -1,7 +0,0 @@
-// Малюємо стільки котиків, скільки захочемо!
-var drawCats = function (howManyTimes) {
-    for (var i = 0; i < howManyTimes; i++) {
-        console.log(i + " =^.^=");
-    }
-};
-drawCats(10);
