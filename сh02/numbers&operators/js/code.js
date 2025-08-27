423870dd8cac77abe63c75b32db39481cb586424
Nick Davshan <spacestark84@gmail.com>
2025-08-27 11:32:23 +0300

    Update code.js

---
+++ сh02/numbers&operators/js/code.js
@@ -0,0 +1,15 @@
+console.log(12345 + 56789);
+console.log(22 + 33 + 44);
+console.log(1000 - 17);
+console.log(123 * 456);
+console.log(123 / 250);
+console.log(1234 + (57 * 3) - (31 / 4));
+console.log(8 / 1 + 3);
+console.log(8 / (1 + 3));
+console.log((15 + 9) * 2);
+
+
+
+
+
+
