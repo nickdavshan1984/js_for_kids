d0339f637bb97df9bcac3b63a8e53e9871b48249
Nick Davshan <spacestark84@gmail.com>
2025-08-28 10:07:21 +0300

    Update code.js

--- сh02/variables/js/code.js
+++ сh02/variables/js/code.js
@@ -1,3 +1,10 @@
+//Задача с конфетами
 var numberOfSiblings = 1 + 3;
 var numberOfCandies = 8;
 console.log(numberOfCandies / numberOfSiblings);
+
+//Задача с секундами
+var secondsInAMinute = 60;
+var minutesInAnHour = 60;
+var secondsInAnHour = secondsInAMinute * minutesInAnHour;
+console.log(secondsInAnHour);
