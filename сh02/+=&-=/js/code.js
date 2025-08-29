3ca2aa1c7a4f7517569ff0389f40d00dd2ed91c2
Nick Davshan <spacestark84@gmail.com>
2025-08-29 12:29:16 +0300

    add_new_+=&-=_files

---
+++ сh02/+=&-=/js/code.js
@@ -0,0 +1,7 @@
+//Программа подсчёта очков в игре
+console.log("Подсчёт очков в игре");
+var score = 10;
+score += 7;
+console.log("Количество очков: ", score);
+score -= 3;
+console.log("Количество очков: ", score);
