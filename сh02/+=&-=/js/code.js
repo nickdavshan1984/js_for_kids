7b2c0235e6a624a60adb81184fedcaf49bf28372
Nick Davshan <spacestark84@gmail.com>
2025-08-29 12:40:03 +0300

    Update code.js

--- сh02/+=&-=/js/code.js
+++ сh02/+=&-=/js/code.js
@@ -5,3 +5,11 @@ score += 7;
 console.log("Количество очков: ", score);
 score -= 3;
 console.log("Количество очков: ", score);
+console.log("");
+
+//Попробуйте!
+console.log("Задание 'Попробуйте!'");
+var baloons = 100;
+console.log(baloons *= 2);
+baloons = 100;
+console.log(baloons /= 4);
